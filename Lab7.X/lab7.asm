6bb054647327253f59cf49450b8a3fae95fbe2c2
aben20807 <aben20807@gmail.com>
2017-11-19 11:01:37 +0800

    Lab7

---
+++ Lab7.X/lab7.asm
@@ -0,0 +1,135 @@
+LIST p=18f4520		
+#include <p18f4520.inc>		
+; CONFIG1H
+  CONFIG  OSC = INTIO67         ; Oscillator Selection bits (Internal oscillator block, port function on RA6 and RA7)
+  CONFIG  FCMEN = OFF           ; Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+  CONFIG  IESO = OFF            ; Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+; CONFIG2L
+  CONFIG  PWRT = OFF            ; Power-up Timer Enable bit (PWRT disabled)
+  CONFIG  BOREN = OFF           ; Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+  CONFIG  BORV = 0              ; Brown Out Reset Voltage bits (Maximum setting)
+
+; CONFIG2H
+  CONFIG  WDT = OFF             ; Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+  CONFIG  WDTPS = 1             ; Watchdog Timer Postscale Select bits (1:1)
+
+; CONFIG3H
+  CONFIG  CCP2MX = PORTBE       ; CCP2 MUX bit (CCP2 input/output is multiplexed with RB3)
+  CONFIG  PBADEN = OFF          ; PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+  CONFIG  LPT1OSC = OFF         ; Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+  CONFIG  MCLRE = ON            ; MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+; CONFIG4L
+  CONFIG  STVREN = ON           ; Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+  CONFIG  LVP = OFF             ; Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+  CONFIG  XINST = OFF           ; Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+; CONFIG5L
+  CONFIG  CP0 = OFF             ; Code Protection bit (Block 0 (000800-001FFFh) not code-protected)
+  CONFIG  CP1 = OFF             ; Code Protection bit (Block 1 (002000-003FFFh) not code-protected)
+  CONFIG  CP2 = OFF             ; Code Protection bit (Block 2 (004000-005FFFh) not code-protected)
+  CONFIG  CP3 = OFF             ; Code Protection bit (Block 3 (006000-007FFFh) not code-protected)
+
+; CONFIG5H
+  CONFIG  CPB = OFF             ; Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+  CONFIG  CPD = OFF             ; Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+; CONFIG6L
+  CONFIG  WRT0 = OFF            ; Write Protection bit (Block 0 (000800-001FFFh) not write-protected)
+  CONFIG  WRT1 = OFF            ; Write Protection bit (Block 1 (002000-003FFFh) not write-protected)
+  CONFIG  WRT2 = OFF            ; Write Protection bit (Block 2 (004000-005FFFh) not write-protected)
+  CONFIG  WRT3 = OFF            ; Write Protection bit (Block 3 (006000-007FFFh) not write-protected)
+
+; CONFIG6H
+  CONFIG  WRTC = OFF            ; Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+  CONFIG  WRTB = OFF            ; Boot Block Write Protection bit (Boot block (000000-0007FFh) not write-protected)
+  CONFIG  WRTD = OFF            ; Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+; CONFIG7L
+  CONFIG  EBTR0 = OFF           ; Table Read Protection bit (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+  CONFIG  EBTR1 = OFF           ; Table Read Protection bit (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+  CONFIG  EBTR2 = OFF           ; Table Read Protection bit (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+  CONFIG  EBTR3 = OFF           ; Table Read Protection bit (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+; CONFIG7H
+  CONFIG  EBTRB = OFF           ; Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
+
+;********************************************************************
+;****	RESET Vector @ 0x0000
+;********************************************************************
+    org		0x0000 		;  
+    BRA		Main
+
+    org		0x0008		; 
+    ;call Hi_ISRs,FAST
+    ;retfie  FAST
+    BRA		Hi_ISRs
+    
+;********************************************************************
+;****	The Main Program start from Here !! 
+;********************************************************************
+    org		0x0020
+Main:
+    CALL    Init_IO
+    CALL    Init_INT0
+    BSF     RCON, IPEN                   ; Enable Interrupt priority
+    BSF     INTCON, GIEH                 ; Enable all High Priority Interrupt
+
+Null_Loop	GOTO	Null_Loop		; Do loop here
+
+;***********************************************************************
+;****		Initial the PORTD for the output port 
+;***********************************************************************
+Init_IO:
+    CLRF	PORTB
+    BSF     TRISB,0         ; 00000001: RB0 input
+    CLRF	PORTD
+    CLRF    TRISD           ; Set PORTD for LED output
+    RETURN
+    
+;***********************************************************************
+;****		Initial INTCON, INTCON2
+;***********************************************************************   
+Init_INT0:
+    MOVLW	B'10010000'				
+    MOVWF	INTCON          ; Enable Interrupt, Clear Interrupt flag
+    MOVLW	B'00000000'				
+    MOVWF	INTCON2
+    RETURN
+    
+;***********************************************************************
+;****		Delay macro
+;***********************************************************************
+    L1  EQU 0X14
+    L2  EQU 0X15
+DELAY MACRO num1,num2
+    local LOOP1         ;prevent compile error
+    local LOOP2               
+    MOVLW num2
+    MOVWF L2
+    LOOP2:
+	MOVLW num1                   
+	MOVWF L1  
+    LOOP1:
+	NOP
+	NOP
+	NOP
+	NOP
+	DECFSZ L1,1
+	    GOTO LOOP1
+	DECFSZ L2,1
+	    GOTO LOOP2
+    ENDM
+    
+;***************************************************************************************
+;****		ISRs() : 中斷服務程式
+;***************************************************************************************
+Hi_ISRs
+    NOP
+    BCF		INTCON, INT0IF      ; Clear Interrupt flag
+    BSF     LATD, 0, 0
+    DELAY   0x4f, 0x4f
+    CLRF    LATD
+    RETFIE  FAST                ; Return with shadow register
+    END
